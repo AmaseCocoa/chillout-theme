13c1483b3f637b7689e4d17663ec27fc1ad136f4
甘瀬ここあ <amase.cocoa@gmail.com>
2024-11-19 18:31:14 +0900

    Create tailwind.config.js

---
+++ v1/extras/tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        // Light Theme
+        light: {
+          bg: '#F0F0F0',
+          fg: '#5f5f5f',
+          link: '#3B8DA0',
+          love: '#dd2e44',
+          warn: '#ecb637',
+          error: '#ec4137',
+          success: '#86b300',
+          badge: '#31b1ce',
+          panel: 'rgba(0, 0, 0, 0.05)',
+          accent: '#A3D9D1',
+          shadow: 'rgba(0, 0, 0, 0.1)',
+        },
+        // Dark Theme
+        dark: {
+          bg: '#2C2C2C',
+          fg: '#E0E0E0',
+          link: '#44a4c1',
+          love: '#dd2e44',
+          warn: '#ecb637',
+          error: '#ec4137',
+          success: '#86b300',
+          badge: '#31b1ce',
+          panel: 'rgba(255, 255, 255, 0.05)',
+          accent: '#A3D9D1',
+          shadow: 'rgba(0, 0, 0, 0.3)',
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
